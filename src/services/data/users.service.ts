68eae217669ab77381659285a7f13757452085c6
Ivan L. Seibel <me@ivanseibel.dev>
2021-08-30 14:19:07 -0300

    Create users service

---
+++ src/services/data/users.service.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+import { User } from '../../types/server.data.types';
+
+const fetchUsers = async () => {
+    const response = await axios.get('http://localhost:3004/userss');
+    const { data } = response;
+
+    return data;
+}
+
+export function useFetchUsers() {
+    return useQuery<User[], Error>('users', fetchUsers)
+}
