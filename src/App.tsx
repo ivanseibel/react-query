a2de92fb31aa70092a322d6513fd2dfd4a9fcf32
Ivan L. Seibel <me@ivanseibel.dev>
2021-08-30 14:17:34 -0300

    Implement first ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { QueryClientProvider, QueryClient } from "react-query";
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,31 +6,37 @@ import {
   Link
 } from "react-router-dom";
 
+import { useFetchUsers } from './services/data/users';
+
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+    <QueryClientProvider client={queryClient} >
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Switch>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
@@ -39,5 +45,31 @@ function Home() {
 }
 
 function Users() {
-  return <h2>Users</h2>;
+  const {
+    data: users,
+    isLoading,
+    isError,
+    error
+  } = useFetchUsers();
+
+  return (
+    <div>
+      <h2>Users</h2>
+      {isLoading && (
+        <span>...loading data</span>
+      )}
+
+      {isError && error && (
+        <span>{error.message}</span>
+      )}
+
+      {typeof users === 'object' && (
+        users.map(user => (
+          <div>
+            <span>{user.id}</span>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
